a312711d22e4bf2f539c9a4c95a6f988052629c3
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2024-07-07 16:22:29 +0200

    feat: redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, incriment } from './redux/user/userSlice'
 
+export default function App() {
+  const count = useSelector(state => state.counter.value)
+  const dispatch = useDispatch()
 
-
-function App() {
   return (
-    <div className="App">
-     
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incriment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
